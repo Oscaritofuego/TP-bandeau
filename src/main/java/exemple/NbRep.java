4ce33f6d33a4cbaba6eabf42377adec4c14be5ca
Oscaritofuego <123547828+Oscaritofuego@users.noreply.github.com>
2023-12-05 11:23:20 +0100

    Create NbRep.java

---
+++ src/main/java/exemple/NbRep.java
@@ -0,0 +1,14 @@
+package TP;
+
+public class NbRep {
+
+    public int nbRep;
+
+    public NbRep(int nbRep) {
+        this.nbRep = nbRep;
+    }
+
+    public int getNbRep() {
+        return nbRep;
+    }
+}
